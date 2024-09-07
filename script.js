85eb5973d42b040e0451fe3c4abf8fe55aa20e3d
Levy <levyfernandes14@gmail.com>
2024-09-06 23:07:31 -0300

    Word analyzing function done, general code execution incomplete

--- script.js
+++ script.js
@@ -2,23 +2,25 @@ const mainContainer = document.getElementById("main-container");
 const inputElement = document.getElementById("user-input");
 const submitButton = document.getElementById("submit-button");
 
-const actualWord = "SPACE";
-actualWord.toUpperCase().split("");
+const word = "SPACE";
 
-let attempts = 0;
+const user = {
+    attempts: 0,
+    input: ""
+}
 
-function wordContainer(word) {
-    const container = document.createElement('div');
-    container.className = "flex justify-evenly items-center w-full h-1/6 rounded-lg"
+function displayWord() {
+    const wordElement = document.createElement('div');
+    wordElement.className = "flex justify-evenly items-center w-full h-1/6 rounded-lg"
 
-    for (let i = 0; i < word.length; i++) {
-        const letterContainer = document.createElement('div');
-        letterContainer.className = "flex justify-center items-center w-1/6 h-3/4 bg-gray-700 rounded-xl text-gray-200 font-semibold text-3xl box-border select-none";
-        letterContainer.textContent = word[i];
-        container.appendChild(letterContainer);          
+    for (let i = 0; i < 5; i++) {
+        const letterElement = document.createElement('div');
+        letterElement.className = "flex justify-center items-center w-1/6 h-3/4 bg-gray-700 rounded-xl text-gray-200 font-semibold text-3xl box-border select-none";
+        letterElement.textContent = user.input[i];
+        wordElement.appendChild(letterElement);          
     }
 
-    mainContainer.appendChild(container);
+    mainContainer.appendChild(wordElement);
 }
 
 function sanitize(input) {
@@ -27,38 +29,34 @@ function sanitize(input) {
     return word;
 }
 
-window.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        const userInput = sanitize(inputElement.value);
-        
-        if (userInput.length == 5 && attempts < 6) {
-            inputElement.value = "";
-            wordContainer(userInput);
-            attempts++;
+function checkWord() {
+    for (let i in user.input) {
+        if (user.input[i] === word[i]) {
+            console.log(user.input[i], "GREEN");
+        } else if (word.includes(user.input[i]) && user.input[i] !== word[i]) {
+            console.log(user.input[i], "YELLOW")
+        } else if (user.input[i] != word[i]) {
+            console.log(user.input[i], "RED");
         }
     }
-})
+}
 
-submitButton.addEventListener("click", function() {
-    const userInput = sanitize(inputElement.value);
-        
-        if (userInput.length == 5 && attempts < 6) {
-            inputElement.value = "";
-            wordContainer(userInput);
-            attempts++;
-        }
+function validateInput() {
+    user.input = sanitize(inputElement.value);
+    if (user.input.length == 5 && user.attempts < 6) {
+        checkWord();
+        displayWord(user.input);
+        user.attempts += 1;
+        inputElement.value = "";
+    }
+}
+
+window.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        validateInput();
+    }
 });
 
-
-
-// word is max. 5 letters
-// 6 chances
-// sanitize word before manipulating it
-
-/* additional features:
-
-// json with a lot of 5 letter words 
-// pick one random word from json each day
-
-
-*/
+submitButton.addEventListener("click", function() {
+    validateInput(); 
+});
